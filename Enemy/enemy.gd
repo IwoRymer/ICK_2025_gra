2950763ca4d1214a8aeff36611ac82a74cfd52bc
AdamJedrz <adamjedrzejczyk990@gmail.com>
2025-04-03 17:50:17 +0200

    creation of 5 types of enemies

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,13 +2,15 @@ extends CharacterBody2D
 
 @export var movement_speed = 20.0
 @export var hp = 10
-
+@export var damage = 1
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var sprite = $Sprite2D
 @onready var animation  = $AnimationPlayer
+@onready var hitBox = $HitBox
 
 func _ready():
 	animation.play("walk")
+	hitBox.damage = damage
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
