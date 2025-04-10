ae3805b38666fe792eb42ff658544f02efd1654b
pkaminski@student.agh.edu.pl <103180611+PKaminski1946@users.noreply.github.com>
2025-04-10 10:06:08 +0200

    healing+comm

--- Player/player.gd
+++ Player/player.gd
@@ -6,8 +6,12 @@ extends CharacterBody2D
 var movement_speed = 40.0
 var hp = 80
 
+var healing_amount = 10
+var healing_cooldown = 5 #s
+
 @onready var sprite = $Sprite2D
 @onready var walkTimer = get_node("%walkTimer")
+@onready var healTimer = get_node("%healTimer")
 
 ## delta default = 1/60 s
 
@@ -16,7 +20,11 @@ func _physics_process(delta: float) -> void:
 	
 func _ready():
 	add_to_group("player")
+	healTimer.wait_time = healing_cooldown
 
+func _process(delta):
+	if Input.is_action_just_pressed("heal"):
+		heal(healing_amount)
 
 func movement():
 	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
@@ -48,3 +56,20 @@ func _on_hurt_box_hurt(damage: Variant) -> void:
 func take_damage(dmg: int):
 	hp -= dmg
 	print("pocisk ",hp)
+
+func heal(dmg: int):
+	if healTimer.is_stopped():
+		hp += dmg
+		sprite.self_modulate.g = 0.3
+		sprite.self_modulate.b = 0.3
+		
+		healTimer.start()
+		
+func _on_heal_timer_timeout() -> void:
+	sprite.self_modulate.g = 1
+	sprite.self_modulate.b = 1
+
+func _on_udp_server_client_control(control_vals: Variant) -> void:
+	if control_vals[0] != null:
+		if control_vals[0][0] == "1":
+			heal(healing_amount)
